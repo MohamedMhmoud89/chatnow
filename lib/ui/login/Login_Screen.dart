1a7586c9958d2f279ca62ed45143c75aca23dde2
Mohamed Mahmouad <mcapo416@gmail.com>
2024-12-27 02:58:47 +0200

    develop branch

--- lib/ui/login/Login_Screen.dart
+++ lib/ui/login/Login_Screen.dart
@@ -21,76 +21,80 @@ class LoginScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Login'),
         ),
-        body: Padding(
-          padding: EdgeInsets.symmetric(vertical: 210, horizontal: 20),
-          child: Column(
-            spacing: 30,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Welcome back!',
-                textAlign: TextAlign.start,
-                style: GoogleFonts.poppins(
-                    fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              CustomTextFormField(
-                validator: (text) {
-                  if (text == null || text.trim().isEmpty) {
-                    return "Please enter email";
-                  }
-                  if (!ValidationUtils.isValidEmail(text)) {
-                    return "email bad format";
-                  }
-                  return null;
-                },
-                labelText: "Email",
-                controller: TextEditingController(),
-                textInputType: TextInputType.emailAddress,
-              ),
-              CustomTextFormField(
-                validator: (text) {},
-                labelText: "Password",
-                controller: TextEditingController(),
-                isPassword: true,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, HomeScreen.routeName);
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Login',
-                      style: GoogleFonts.poppins(
-                          fontSize: 14,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_rounded,
-                      color: Colors.white,
-                      size: 30,
-                    ),
-                  ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 210, horizontal: 20),
+            child: Column(
+              spacing: 30,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Welcome back!',
+                  textAlign: TextAlign.start,
+                  style: GoogleFonts.poppins(
+                      fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6)),
-                    backgroundColor: Theme.of(context).primaryColor,
-                    padding:
-                        EdgeInsets.symmetric(vertical: 18, horizontal: 40)),
-              ),
-              Row(
-                children: [
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, RegisterScreen.routeName);
-                      },
-                      child: Text('Or Create My Account'))
-                ],
-              )
-            ],
+                CustomTextFormField(
+                  validator: (text) {
+                    if (text == null || text.trim().isEmpty) {
+                      return "Please enter email";
+                    }
+                    if (!ValidationUtils.isValidEmail(text)) {
+                      return "email bad format";
+                    }
+                    return null;
+                  },
+                  labelText: "Email",
+                  controller: TextEditingController(),
+                  textInputType: TextInputType.emailAddress,
+                ),
+                CustomTextFormField(
+                  validator: (text) {},
+                  labelText: "Password",
+                  controller: TextEditingController(),
+                  isPassword: true,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(
+                        context, HomeScreen.routeName);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Login',
+                        style: GoogleFonts.poppins(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_rounded,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6)),
+                      backgroundColor: Theme.of(context).primaryColor,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 18, horizontal: 40)),
+                ),
+                Row(
+                  children: [
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, RegisterScreen.routeName);
+                        },
+                        child: Text('Or Create My Account'))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
